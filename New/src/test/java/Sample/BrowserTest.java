b6e5f839a65f8ee188b94c0b0e69f8b72000aea0
SRIDHAR <SRIDHAR@LAPTOP-H2MSSOTG>
2023-03-04 17:16:27 +0530

    Test flow

---
+++ New/src/test/java/Sample/BrowserTest.java
@@ -0,0 +1,13 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class BrowserTest {
+	
+	@Test
+	public void testRun()
+	{
+		System.out.println("maven setup is completed");
+	}
+
+}
---
+++ New/src/test/java/Sample/UserTest.java
@@ -0,0 +1,12 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class UserTest {
+
+	@Test
+	public void runTest()
+	{
+		System.out.println("Maven flow is perfect");
+	}
+}
